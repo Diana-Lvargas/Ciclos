bf70f17ae661d3ac33e0a821c47168aa9cfe2d58
Diana-Lvargas <dvargas11@ucol.mx>
2019-08-24 18:32:28 -0500

    Hice el codigo

---
+++ Ciclo for/Ciclo for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ciclo_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Inserta el numero con el que inicia el ciclo:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserta el numero con el que finaliza el ciclo:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Inserta el numero con el que quieres avance:");
+            c = Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i <= b; i += c)
+            {
+                Console.WriteLine("i = " + i);
+            }
+        }
+    }
+}
